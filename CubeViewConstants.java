155e3dc395299c48da45c5045699a98580557a10
7 Thunders <seventhundersmobile@gmail.com>
2022-03-30 19:21:47 -0400

    Add files via upload
    CubeViewConstants.java includes final integers which are shared between a few files in the app. It seemed best to centralize the shared values.

---
+++ CubeViewConstants.java
@@ -0,0 +1,10 @@
+package com.seventhundersoftware.cubeview;
+
+public final class CubeViewConstants {
+    static final int I_ISLANDS = 0;
+    static final int I_LIGHTHOUSE = 1;
+    static final int I_RIVER = 2;
+    static final int I_GALLERY = 3;
+    static final int I_GRID = 4;
+    static final int I_IMAGES = 5;
+}
